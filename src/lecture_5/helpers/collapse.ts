e06e2e77635214859cfa289357e6c2f46dbed09d
Veniamin Trepachko <trepachko@tochka.com>
2023-04-21 01:09:38 +0400

    feat: Добавил описание и вынес тип

--- src/lecture_5/helpers/collapse.ts
+++ src/lecture_5/helpers/collapse.ts
@@ -1,4 +1,6 @@
-const addKeyToPrefix = (key: string, prefix?: string) => (prefix ? `${prefix}.${key}` : key);
+type TCollapseResult = Record<string, string | boolean | number | null | undefined>;
+
+const addKeyToPrefix = (key: string, prefix?: string): string => (prefix ? `${prefix}.${key}` : key);
 
 /**
  * Рекурсивно «сжимает» переданный вложенный объект в плоский вид
@@ -6,8 +8,8 @@ const addKeyToPrefix = (key: string, prefix?: string) => (prefix ? `${prefix}.${
  * @param data Объект, который нужно сжать
  * @returns Сжатый объект
  */
-export const recursiveCollapse = (data: Object): Record<string, string | boolean | number> => {
-	const result: Record<string, string | number | boolean> = {};
+export const recursiveCollapse = (data: Object): TCollapseResult => {
+	const result: TCollapseResult = {};
 
 	function collapseHelper(data: any, property: string = '') {
 		if (typeof data !== 'object' || data === null) {
@@ -24,10 +26,16 @@ export const recursiveCollapse = (data: Object): Record<string, string | boolean
 	return result;
 };
 
-export const collapse = (data: Object): Record<string, string | boolean | number | null | undefined> => {
+/**
+ * «Сжимает» переданный вложенный объект в плоский вид используя стек
+ *
+ * @param data Объект, который нужно сжать
+ * @returns Сжатый объект
+ */
+export const collapse = (data: Object): TCollapseResult => {
 	const stack: Array<any> = [data];
 	const properties: Array<string> = [];
-	const result: Record<string, string | boolean | number | null | undefined> = {};
+	const result: TCollapseResult = {};
 
 	while (stack.length) {
 		const element = stack.pop();
